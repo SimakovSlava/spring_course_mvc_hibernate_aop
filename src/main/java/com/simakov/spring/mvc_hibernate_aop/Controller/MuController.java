8bd2f2abb8f9b9c8c2bcd271e5eb2ca5d00fd6a1
Vyacheslav <slava_94@mail.ru>
2023-01-13 16:47:29 +0300

    adding new employee

--- src/main/java/com/simakov/spring/mvc_hibernate_aop/Controller/MuController.java
+++ src/main/java/com/simakov/spring/mvc_hibernate_aop/Controller/MuController.java
@@ -3,9 +3,11 @@ package com.simakov.spring.mvc_hibernate_aop.Controller;
 import com.simakov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.simakov.spring.mvc_hibernate_aop.entity.Employee;
 import com.simakov.spring.mvc_hibernate_aop.service.EmployeeService;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +24,16 @@ public class MuController {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmpls", allEmployees);
         return "all-employees";
+    }
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+    @RequestMapping("saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
