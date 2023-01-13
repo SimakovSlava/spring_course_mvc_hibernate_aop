60ad8381331b71cff678ea3dc62f44f10462e4af
Vyacheslav <slava_94@mail.ru>
2023-01-13 21:20:53 +0300

    add "update" Employee

--- src/main/java/com/simakov/spring/mvc_hibernate_aop/Controller/MuController.java
+++ src/main/java/com/simakov/spring/mvc_hibernate_aop/Controller/MuController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,21 +20,30 @@ public class MuController {
     private EmployeeService employeeService;
 
     @RequestMapping("/")
-    public String showAllEmployees(Model model){
+    public String showAllEmployees(Model model) {
 
         List<Employee> allEmployees = employeeService.getAllEmployees();
-        model.addAttribute("allEmpls", allEmployees);
+        model.addAttribute("allEmps", allEmployees);
         return "all-employees";
     }
+
     @RequestMapping("/addNewEmployee")
-    public String addNewEmployee(Model model){
+    public String addNewEmployee(Model model) {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "employee-info";
     }
-    @RequestMapping("saveEmployee")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.saveEmployee(employee);
         return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "employee-info";
     }
 }
